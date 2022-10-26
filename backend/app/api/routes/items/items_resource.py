3f4ef2987fa985cdb4fdcd58367a135a9bca5c55
Jesse Favelle <jesse.favelle@mavtek.com>
2022-10-26 14:22:12 -0500

    Fixed Image

--- backend/app/api/routes/items/items_resource.py
+++ backend/app/api/routes/items/items_resource.py
@@ -68,7 +68,7 @@ async def create_new_item(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=strings.ITEM_ALREADY_EXISTS,
         )
-    if item_create.image == "":
+    if not item_create.image:
         item_create.image = 'placeholder.png'
     item = await items_repo.create_item(
         slug=slug,
