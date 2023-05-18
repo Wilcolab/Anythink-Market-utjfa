9e39ebc232d239acb82af2aacb040239156414d5
jfavelleMavtek <108832877+jfavelleMavtek@users.noreply.github.com>
2023-05-18 16:41:02 +0000

    exportDefault

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,7 +49,7 @@ const LoggedInView = (props) => {
   );
 };
 
-export const Header = (props) => {
+ const Header = (props) => {
     return (
       <nav
         className="navbar navbar-expand-md navbar-dark"
@@ -66,4 +66,7 @@ export const Header = (props) => {
         )}
       </nav>
     );
+    
 }
+
+export default Header
